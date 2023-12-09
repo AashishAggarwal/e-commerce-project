bd4b0af8c18322b24eab0447df07e54bea42d214
Aashish <aggarwal.aashish13@gmail.com>
2023-12-10 00:38:22 +0530

    Add database dependency and test case

---
+++ src/test/java/com/productservice/productservice/controllers/ProductControllerTest.java
@@ -0,0 +1,13 @@
+package com.productservice.productservice.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductControllerTest {
+    
+    @Test
+    void testAddition() {
+        assert(1 + 1 == 2);
+    }
+}
